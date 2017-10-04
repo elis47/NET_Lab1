48f71e545e8f45d633794f501b85190a0b946b94
Elisa <elisa.aioanei@yahoo.com>
2017-10-04 23:17:38 +0300

    Add project files.

---
+++ NET_Lab1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NET_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
